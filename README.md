efa04fd5a8b657b023f93a47127ae2ba5ec99a21
Martynas <68890393+martynaszuravliovas@users.noreply.github.com>
2024-07-25 14:39:01 +0300

    initial commit

---
+++ README.md
@@ -0,0 +1,70 @@
+# Codefights.net SDK for Python
+
+### About
+Visit [codefights.net](http://www.codefights.net/) to learn more about this fun project.
+
+### Installation
+Download this package to /your/desired/path.
+
+Install it:
+pip3 install codefights
+
+### Writing your first bot
+Developing a simple bot for a Codefights.net tournament is really easy.
+
+We have prepared a simple demo bot - save it as YourFighterName.py and you are ready!
+
+```
+from codefights.model.IFighter import *
+import codefights.boilerplate.SDK
+import sys
+
+class MyFighter (IFighter):
+    """
+    analyze your opponent's last move and make your next move
+    :returns fighter's next Move
+    NOTE: rules allow max 3 actions per Move.
+    I.e. attack nose (1), attack groin (2) and defend nose (3).
+    The areas are:
+    +------------+---------+
+    | Area.NOSE  | (10pts) |
+    |------------+---------|
+    | Area.JAW   | (8pts)  |
+    |------------+---------|
+    | Area.BELLY | (6pts)  |
+    |------------+---------|
+    | Area.GROIN | (4pts)  |
+    |------------+---------|
+    | Area.LEGS  | (3pts)  |
+    +------------+---------+
+    """
+
+    def make_next_move(self,
+                       opponents_last_move=None,
+                       my_last_score=0,
+                       opponents_last_score=0):
+        """
+        You must implement make_next_move method in MyFighter class.
+        Feel free to create helper classes in this file.
+        """
+        move = Move()
+
+        move.add_attack(Area.NOSE).add_block(Area.GROIN).add_attack(Area.BELLY)
+
+        return move
+
+
+# DO NOT EDIT THE LINES BELOW!
+if __name__ == '__main__':
+    codefights.boilerplate.SDK.SDK.run(MyFighter, sys.argv)
+```
+
+### Testing your bot
+Run it
+```
+python3 </path/to/fighter/>YourFighterName.py
+```
+and follow instructions.
+
+---
+©  twoday
---
+++ codefights/boilerplate/Commentator.py
@@ -0,0 +1,87 @@
+from codefights.model.GameScoringRules import GameScoringRules
+from codefights.boilerplate.server.Protocol import Protocol
+
+class Commentator:
+
+    def __init__(self):
+        self._fighter1 = 'Fighter1'
+        self._fighter2 = 'Fighter2'
+
+        self._lp1 = GameScoringRules().LIFEPOINTS
+        self._lp2 = GameScoringRules().LIFEPOINTS
+
+    def set_fighter_names(self, fighter1name, fighter2name):
+        self._fighter1 = fighter1name
+        self._fighter2 = fighter2name
+
+    def describe_round(self, move1, move2, score1, score2):
+        self._describe_move(self._fighter1, move1, score1, move2)
+        self._describe_move(self._fighter2, move2, score2, move1)
+
+        self._lp1 -= score2
+        self._lp2 -= score1
+
+        print(f'{self._fighter1} vs {self._fighter2}: {self._lp1} to {self._lp2}\n')
+
+
+    def game_over(self, f1_lifepoints, f2_lifepoints):
+        print('FIGHT OVER')
+
+        if f1_lifepoints > f2_lifepoints:
+            print(f'THE WINNER IS {self._fighter1}')
+
+        elif f2_lifepoints > f1_lifepoints:
+            print(f'THE WINNER IS {self._fighter2}')
+
+        else:
+            print("IT'S A DRAW!!!")
+
+    @staticmethod
+    def _describe_move(fighter_name, move, score, counter_move):
+        print(f'{fighter_name}'
+              f'{Commentator._describe_attacks(move, counter_move, score)}'
+              f'{Commentator._describe_defences(move)}'
+              f'{Commentator._describe_comment(move)}')
+
+    @staticmethod
+    def _describe_attacks(move, counter_move, score):
+        attacks = move.get_attacks()
+
+        if len(attacks) <= 0:
+            return ' did NOT attack at all '
+
+        rez = ' attacked '
+
+        for attack in attacks:
+            rez += attack
+
+            blocked = attack in counter_move.get_blocks()
+
+            if blocked:
+                rez += '(-), '
+            else:
+                rez += '(+), '
+
+        return '%s %s %s ' % (rez, 'scoring', score)
+
+    @staticmethod
+    def _describe_defences(move):
+        blocks = move.get_blocks()
+
+        if len(blocks) <= 0:
+            return ' and was NOT defending at all.'
+
+        rez = ' while defending '
+        for block in blocks:
+            rez += block + ', '
+
+        return rez
+
+    @staticmethod
+    def _describe_comment(move):
+        comment = move.get_comment()
+
+        if not comment or comment is None or len(comment) <= 0:
+            return ''
+
+        return ' Also said "%s"' % Protocol.sanitize_comment(comment)
